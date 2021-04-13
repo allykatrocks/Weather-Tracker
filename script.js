d817382e718d932ffb82bb925fbc4024c114952c
Alex DeMarco <demarcoalexandria5@gmail.com>
2021-04-12 22:36:49 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var accessCityForm = document.getElementById("search-cities");
+var inputCity = document.getElementById("city");
+var key = "e37b1c350cc530571e0d70e62fce1f52";
+
+accessCityForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    var inputCityValue = inputCity.value;
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + inputCityValue + "&limit=1&appid=" + key).then(function(response) {
+        return response.json().then(function(data) {
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + key).then(function(outcome) {
+                return outcome.json().then(function(weatherData) {
+                    console.log(weatherData);
+                });
+            });
+
+        })
+    })
+})
+
+
+
+
+
+
+
