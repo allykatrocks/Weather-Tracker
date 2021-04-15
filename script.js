47c12b740f04e1e1a28ab25bc8bfbe91d400817a
Alex DeMarco <demarcoalexandria5@gmail.com>
2021-04-14 17:25:49 -0700

    actual progress

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var accessCityForm = document.getElementById("search-cities");
 var inputCity = document.getElementById("city");
 var key = "e37b1c350cc530571e0d70e62fce1f52";
+var dateEl = document.querySelector(".card-title");
+var tempEl = document.getElementById("temp");
+var humidEl = document.getElementById("humid");
+var windEl = document.getElementById("wind");
+var uviEl = document.getElementById("uvi");
 
 
 accessCityForm.addEventListener("submit", function(event) {
@@ -12,10 +17,12 @@ accessCityForm.addEventListener("submit", function(event) {
             var lon = data[0].lon;
             fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + key).then(function(outcome) {
                 return outcome.json().then(function(weatherData) {
-                    displayData(weatherData);
-                }).catch(error => {
-                    alert("Error!");
-                    console.log(error);
+                   tempEl.append(weatherData.daily[0].temp.day);
+                   humidEl.append(weatherData.daily[0].humidity);
+                   windEl.append(weatherData.daily[0].wind_speed);
+                   uviEl.append(weatherData.daily[0].uvi);
+                    var date = moment.unix(weatherData.current.dt).format("MM/DD/YYYY");
+                    dateEl.textContent = date;
                 })
             });
 
@@ -23,9 +30,9 @@ accessCityForm.addEventListener("submit", function(event) {
     })
 })
 
-function displayData() {
-    
-}
+//get city and icon to appear
+//fix text content of appends
+//clear past results when new search is initiated
 
 
 
